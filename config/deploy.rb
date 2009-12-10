b7603bda1824c8bf1b1bbba35f2cc137243803b8
Richard Grundy <rich@27smiles.com>
2009-12-10 21:25:44 +0000

    Added yui compressor tasks to deploy.rb.

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,9 +17,30 @@ role :app, "regexme-this.27smiles.com"
 role :web, "regexme-this.27smiles.com"
 role :db, "regexme-this.27smiles.com", :primary => true
 
+set :yui_compressor, "/usr/bin/env java -jar /home/rich/sandbox/yuicompressor.jar"
+
+after "deploy:finalize_update", "minify:javascript"
+after "deploy:finalize_update", "minify:stylesheet"
+
 namespace :deploy do
   desc "restart passenger app"
   task :restart do
     run "touch #{current_path}/tmp/restart.txt"
+  end
+end
+
+namespace :minify do
+  desc "Minifies javascript"
+  task :javascript, :roles => :app do
+    path = "#{latest_release}/public/javascripts"
+    run "#{yui_compressor} --type js #{path}/application.js -o #{path}/application.min.js"
+  end
+
+  desc "Minifies stylesheets"
+  task :stylesheet, :roles => :app do
+    path = "#{latest_release}/public/stylesheets"
+
+    run "#{yui_compressor} --type css #{path}/reset.css -o #{path}/reset.min.css"
+    run "#{yui_compressor} --type css #{path}/master.css -o #{path}/master.min.css"
   end
 end
